d520af866c9d2dd3271b64752b835754920247aa
Dicarbene <1041641731@qq.com>
2022-11-14 14:08:29 +0800

    clean up

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,8 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'woodenfish || 赛博木鱼',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
